465f6e6aee9bfd04064e348ab4dcfe0677270ca6
snahammond <70365904+snahammond@users.noreply.github.com>
2026-04-02 20:27:25 +0100

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,8 +1,19 @@
-@echo off
+@rem
+@rem Gradle startup script for Windows
+@rem
+
+@if "%DEBUG%" == "" @echo off
 set DIR=%~dp0
-set APP_BASE_NAME=%~n0
 set APP_HOME=%DIR%
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+if defined JAVA_HOME (
+    set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+) else (
+    set JAVA_EXE=java.exe
+)
 
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
-"%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
